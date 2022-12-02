91439a380a50f0414721edd28d5ca5008477c037
nathan stephens <nstephens1234567@gmail.com>
2022-12-02 11:05:11 -0600

    initial commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static void Add(int num1, int num2) 
+        {
+            Console.WriteLine(num1 + num2); 
+        }
+        public static void Add(decimal num3, decimal num4) 
+        {
+            Console.WriteLine(num3 + num4);
+        }
+        public static void Add(int num5, int num6, bool word) 
+        {
+            var sum = num5 + num6;  
+            if (word == true && sum > 1)
+            {
+                Console.WriteLine($"{sum} dollars");
+            }
+            else if (word == true && sum == 1)
+                {
+                Console.WriteLine($"{sum} dollar");
+                }
+            else
+            {
+                return;
+            }
+        }
+            
+        
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Methods.Add(37, 14);
+            //Methods.Add(22.9m, 29.6m);
+            Methods.Add(0, 1, true);
         }
     }
 }
